300571ff6b12e5eb77ad2d902e862cf930958ae2
Elle <33027271+ellehallal@users.noreply.github.com>
2022-03-07 21:53:44 +0000

    Setup MarsRover

--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,5 @@
 require_relative './mars_rover'
-require_relative './grid'
-require_relative './coordinates_generator'
+require_relative './input_reader'
 
-coordinates = CoordinatesGenerator.generate(
-  max_x_axis_value: 5,
-  max_y_axis_value: 3
-)
-grid = Grid.new(coordinates: coordinates)
-MarsRover.new(grid: grid)
+file_path = "#{Dir.pwd}/lib/input.txt"
+MarsRover.start(file_path)
--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,5 +1,56 @@
+require_relative './grid'
+require_relative './robot'
+require_relative './orientation'
+require_relative './direction'
+require_relative './move'
+require_relative './coordinates_generator'
+require_relative './input_reader'
+require_relative './input_parser'
+require_relative './output'
+
 class MarsRover
-  def initialize(grid:)
-    @grid = grid
+  class << self
+    def start(file_path)
+      input_parser = create_input_parser(file_path)
+      grid = create_grid(input_parser.grid_size)
+
+      input_parser.instructions.each do |instruction|
+        robot = create_robot(instruction, grid)
+        robot.respond_to_commands(instruction[:commands])
+        Output.print(robot.report_location)
+      end
+    end
+
+    private
+
+    def create_input_parser(file_path)
+      input = InputReader.read(file_path)
+      InputParser.new(input)
+    end
+
+    def create_grid(grid_size)
+      coordinates = CoordinatesGenerator.generate(
+        max_x_axis_value: grid_size[:x_axis],
+        max_y_axis_value: grid_size[:y_axis]
+      )
+
+      Grid.new(coordinates: coordinates)
+    end
+
+    def create_robot(instruction, grid)
+      orientation = Orientation.new(
+        direction: Direction,
+        current_orientation: instruction[:orientation]
+      )
+
+      Robot.new(
+        grid: grid,
+        orientation: orientation,
+        x_axis: instruction[:x_axis],
+        y_axis: instruction[:y_axis],
+        direction: Direction,
+        move: Move
+      )
+    end
   end
 end
