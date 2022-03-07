8a50ee8ec28018f67c00c9743c4ab86fbcf36057
Elle <33027271+ellehallal@users.noreply.github.com>
2022-03-07 22:01:51 +0000

    Setup Mars Rover to take input and output robot locations (#8)

--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,4 @@
 require_relative './mars_rover'
-require_relative './grid'
-require_relative './coordinates_generator'
 
-coordinates = CoordinatesGenerator.generate(
-  max_x_axis_value: 5,
-  max_y_axis_value: 3
-)
-grid = Grid.new(coordinates: coordinates)
-MarsRover.new(grid: grid)
+file_path = "#{Dir.pwd}/lib/input.txt"
+MarsRover.start(file_path)
